fae33a0ea6d0e9b42cb85c437449498680ae41d5
Charles Sanders <charles@primerfp.com>
2025-07-30 13:07:15 -0500

    feat: add gulp-rename plugin for icon build process and update package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 const path = require('path');
-const { task, src, dest } = require('gulp');
+const { task, src, dest, rename } = require('gulp');
+const renamePlugin = require('gulp-rename');
 
 task('build:icons', copyIcons);
 
 function copyIcons() {
-    // Copy icons from resources to both nodes and credentials destinations
+    // Copy icons from resources to dist/nodes and flatten
     const iconSource = path.resolve('resources', '**', '*.{png,svg}');
     const nodesDestination = path.resolve('dist', 'nodes');
 
     return src(iconSource)
+        .pipe(renamePlugin(function (filePath) {
+            // Flatten: only use the basename (vocant.svg)
+            filePath.dirname = '';
+        }))
         .pipe(dest(nodesDestination));
 }
